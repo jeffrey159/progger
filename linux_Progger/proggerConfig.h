13b3b76f8bd3ab586af7e9010b04b02c9a4d91f0
jeffrey159 <jeffrey_ng10@hotmail.com>
2018-10-23 14:36:05 +1300

    Add files via upload

--- linux_Progger/proggerConfig.h
+++ linux_Progger/proggerConfig.h
@@ -7,8 +7,10 @@
  * CONFIGURATION FOR LAYER X BELOW 
  */
 /* Redis server configuration */
+
 //#define REDIS_ADDR ((unsigned long int)0xC0A8A00C) //192.168.160.12	/* TJ org */
-#define REDIS_ADDR ((unsigned long int)0xc0a85d83) //10.0.2.6			/* 31 May TJChoi : could be replaced by argv[]  */
+//#define REDIS_ADDR ((unsigned long int)0x0A000206) //10.0.2.6			/* 31 May TJChoi : could be replaced by argv[]  */
+#define REDIS_ADDR ((unsigned long int)0xc0a8858d) //192.168.133.141
 #define REDIS_PORT 6379
 
 /* Host ID, unique identifier for each host */
